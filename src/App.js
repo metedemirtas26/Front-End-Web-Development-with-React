ecfc46773c9aa1f564aec23ca6579a64ef28a401
metedemirtas26 <metedemirtas26@gmail.com>
2019-02-06 15:09:34 +0100

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
-
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render() {
@@ -13,6 +12,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
